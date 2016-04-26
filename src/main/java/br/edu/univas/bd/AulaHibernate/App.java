faa22cbbaad92180d05d28855334c4e65023e467
Roberto Rocha <rrocha.roberto@gmail.com>
2016-04-25 21:48:47 -0300

    Versão inicial da Aula de Hibernate.

---
+++ src/main/java/br/edu/univas/bd/AulaHibernate/App.java
@@ -0,0 +1,13 @@
+package br.edu.univas.bd.AulaHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
